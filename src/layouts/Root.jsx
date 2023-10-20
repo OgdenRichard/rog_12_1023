a5bfe9af9c3fa81fae1f825a79f53b728d93479b
Richard Ogden <ogden.richard@gmail.com>
2023-10-20 16:21:54 +0200

    20102023 | layouts + folder

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { Navbar } from '../components/Navbar';
-import { Footer } from '../components/Footer';
+import Header from './Header';
+import SideBar from './SideBar';
 
 export const Root = () => {
   return (
     <>
-      <Navbar />
+      <Header />
       <Outlet />
-      <Footer />
+      <SideBar />
     </>
   );
 };
---
+++ src/layouts/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SideBar() {
+  return (
+    <>
+      <div>Sidebaaaart</div>
+    </>
+  );
+}
+
+export default SideBar;
