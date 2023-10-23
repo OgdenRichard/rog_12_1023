b3cd707885e861f5f3782aa83108e598e1c0f49a
Richard Ogden <ogden.richard@gmail.com>
2023-10-23 14:34:19 +0200

    23102023 | display sidebar

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import meditation from '../assets/images/meditation.svg';
+import swimming from '../assets/images/swimming.svg';
+import bike from '../assets/images/bike.svg';
+import dumbbells from '../assets/images/dumbbells.svg';
 
 function SideBar() {
   return (
     <>
-      <div className="sidebar">Sidebaaaar</div>
+      <section className="sidebar">
+        <div className="sidebar__icons">
+          <img src={meditation} alt="logo yoga" />
+          <img src={swimming} alt="logo natation" />
+          <img src={bike} alt="logo cyclisme" />
+          <img src={dumbbells} alt="logo musculation" />
+        </div>
+        <div className="sidebar__copyright">Hé salut ça va?</div>
+      </section>
     </>
   );
 }
