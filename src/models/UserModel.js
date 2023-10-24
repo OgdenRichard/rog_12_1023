43bbe12261683269f3afe8027f658565ebf21d8c
Richard Ogden <ogden.richard@gmail.com>
2023-10-24 16:46:22 +0200

    24102023 | UserModel

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,12 +4,35 @@ export default class UserModel {
   }
 
   setDailyScore = (data) => {
-    this.dailyScore = data.score || data.todayScore;
+    if (data.score || data.todayScore) {
+      this.dailyScore = (data.score || data.todayScore) * 100;
+    }
   };
 
   setKeyData = (data) => {
     if (data.keyData) {
-      this.keyData = data.keyData;
+      this.keyData = {
+        calories: {
+          name: 'Calories',
+          value: data.keyData.calorieCount,
+          unit: 'kCal',
+        },
+        proteins: {
+          name: 'Protéines',
+          value: data.keyData.proteinCount,
+          unit: 'g',
+        },
+        carbohydrates: {
+          name: 'Glucides',
+          value: data.keyData.carbohydrateCount,
+          unit: 'g',
+        },
+        lipids: {
+          name: 'Lipides',
+          value: data.keyData.lipidCount,
+          unit: 'g',
+        },
+      };
     }
   };
 }
