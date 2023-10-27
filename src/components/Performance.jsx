a06162cc0e5623ce1402d379f4f00860956185b9
Richard Ogden <ogden.richard@gmail.com>
2023-10-27 12:31:11 +0200

    27102023 | performance chart tryout

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+} from 'recharts';
 
 function Performance() {
   const performanceData = useFetch(18, 'performance', false);
-  console.log('rendering perf');
+  const chartData = performanceData.data.chartData;
+  console.log(chartData);
   return (
     <>
-      <div>Perf data</div>
+      <div>
+        {chartData && (
+          <RadarChart
+            outerRadius={90}
+            width={730}
+            height={250}
+            data={chartData}
+          >
+            <PolarGrid polarAngles={0} />
+            <PolarAngleAxis dataKey="type" />
+            <PolarRadiusAxis angle={30} domain={[0, 150]} tickCount={1} />
+            <Radar
+              name="Tryout"
+              dataKey="value"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+          </RadarChart>
+        )}
+      </div>
     </>
   );
 }
