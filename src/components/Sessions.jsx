345805616c370282c118c4f447068caea2418e50
Richard Ogden <ogden.richard@gmail.com>
2023-10-30 17:03:06 +0100

    30102023 | sessions chart basic

--- src/components/Sessions.jsx
+++ src/components/Sessions.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Line,
+  Rectangle,
+} from 'recharts';
 
 function Sessions() {
   const sessionsData = useFetch(18, 'average-sessions', false);
-  console.log('rendering sessions');
+  const chartData = sessionsData.data.chartData;
+  console.log(chartData);
+
   return (
     <>
-      <div>Sessions data</div>
+      <div>
+        <LineChart
+          width={300}
+          height={250}
+          data={chartData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="weekday" axisLine={false} tickLine={false} />
+          {/* <YAxis /> */}
+          <Tooltip />
+          <Line
+            dataKey="length"
+            type="monotone"
+            dot={false}
+            strokeWidth={2}
+            stroke="#8884d8"
+          />
+        </LineChart>
+      </div>
     </>
   );
 }
