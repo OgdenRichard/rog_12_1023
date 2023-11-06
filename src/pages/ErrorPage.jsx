4feafe8053bb9cb706479ad0557983f7c51745d8
Richard Ogden <ogden.richard@gmail.com>
2023-11-06 13:50:42 +0100

    06112023 | Error page setup

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,15 +4,13 @@ import { Footer } from '../components/Footer';
 
 export function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
-
   return (
     <>
       <Navbar />
       <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>{error.status}</p>
+        <h1>Erreur</h1>
+        <h2>Sorry, an unexpected error has occurred.</h2>
+        <h2>{error.status}</h2>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
