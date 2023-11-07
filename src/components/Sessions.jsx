399cccc902c455be34ac5a53232feafa678c06ae
Richard Ogden <ogden.richard@gmail.com>
2023-11-07 16:48:20 +0100

    07112023 | fucking line chart

--- src/components/Sessions.jsx
+++ src/components/Sessions.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
-import { LineChart, XAxis, Tooltip, Line, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  XAxis,
+  Tooltip,
+  Line,
+  ResponsiveContainer,
+  YAxis,
+} from 'recharts';
 
 function Sessions({ userId }) {
   const sessionsData = useFetch(userId, 'average-sessions', false);
@@ -15,18 +22,27 @@ function Sessions({ userId }) {
               width={100}
               height={100}
               data={chartData}
-              margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+              margin={{ top: 0, right: 0, left: -60, bottom: 0 }}
             >
+              <YAxis
+                orientation="left"
+                domain={['dataMin', 'dataMax +20']}
+                axisLine={false}
+                tickLine={false}
+                tick={false}
+              />
               <XAxis
                 dataKey="weekday"
                 axisLine={false}
                 tickLine={false}
-                minTickGap={2}
                 interval="preserveStartEnd"
                 tick={{
+                  dx: 0,
                   fontSize: 12,
                   fill: 'white',
                 }}
+                tickMargin={5}
+                padding={{ left: 0, right: 0 }}
               />
               <Tooltip cursor={{ stroke: 'red', strokeWidth: 150 }} />
               <Line
